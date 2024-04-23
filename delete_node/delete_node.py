46c6e711d7eb6578bc490e4695fa747a342dd6ca
EdvardStudent23 <149367334+EdvardStudent23@users.noreply.github.com>
2024-04-23 17:58:48 +0300

    Added a new module for deleting the node. And here there is a screenshot of the passed tests.

---
+++ delete_node/delete_node.py
@@ -0,0 +1,37 @@
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not isinstance(root, TreeNode):
+            return None
+        
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key) 
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key) 
+        else:
+            if not root.right:
+                return root.left
+            elif not root.left:
+                return root.right
+
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.right:
+                return root.left
+            elif not root.left:
+                return root.right
+            
+            suc = root.right
+            par = root
+            while suc.left:
+                suc = suc.left
+                par = suc
+            root.val = suc.val
+
+            if par.right == suc:
+                par.right = self.deleteNode(par.right, suc.val)
+            elif par.left == suc:
+                par.left = self.deleteNode(par.left, suc.val)
+        return root
